59c5e344f6104169c34c5ec3627d6bbd5d659b62
Idhibhat Pankam <marginpankam@gmail.com>
2025-04-10 14:51:03 +0700

    main

---
+++ server/cmd/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"context"
+	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bookpanda/messenger-clone/internal/config"
+	"github.com/bookpanda/messenger-clone/internal/database"
+	"github.com/bookpanda/messenger-clone/internal/jwt"
+	"github.com/bookpanda/messenger-clone/internal/middlewares/authentication"
+	"github.com/bookpanda/messenger-clone/internal/server"
+	"github.com/bookpanda/messenger-clone/internal/services/auth"
+	"github.com/bookpanda/messenger-clone/internal/validator"
+	"github.com/bookpanda/messenger-clone/pkg/logger"
+)
+
+// @title						Pic Me Pls API
+// @version						0.1
+// @description					Pic Me Pls API Documentation
+// @securityDefinitions.apikey ApiKeyAuth
+// @in							header
+// @name						Authorization
+// @externalDocs.description	OpenAPI
+// @externalDocs.url			https://swagger.io/resources/open-api/
+func main() {
+	// hello
+	config := config.Load()
+
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	if err := logger.Init(config.Logger); err != nil {
+		logger.PanicContext(ctx, "failed to initialize logger", slog.Any("error", err))
+	}
+
+	store := database.New(ctx, config.Postgres, config.Redis, config.Storage)
+	server := server.New(config.Server, config.Cors, config.JWT, store)
+	validate := validator.New()
+
+	// services
+	jwtService := jwt.New(config.JWT, store.Cache)
+	// chatService := chat.NewServer(store, validate)
+
+	// middlewares
+	authMiddleware := authentication.NewAuthMiddleware(jwtService)
+
+	// handlers
+	authHandler := auth.NewHandler(store, validate, jwtService, authMiddleware, config.GoogleClientID)
+	// userHandler := user.NewHandler(store, validate, authMiddleware)
+	// objectHandler := objects.NewHandler(store, config.Storage)
+	// messageHandler := message.NewHandler(store, authMiddleware, chatService)
+
+	// routes
+	server.RegisterRoutes(
+		// authMiddleware,
+		authHandler,
+		// userHandler,
+		// objectHandler,
+		// messageHandler,
+	)
+
+	server.Start(ctx, stop)
+}
