89cc9f87f7a442ba8b8e44ad8890ff3e90bdbfac
Idhibhat Pankam <marginpankam@gmail.com>
2025-04-10 14:32:05 +0700

    store

---
+++ server/internal/database/store.go
@@ -0,0 +1,52 @@
+package database
+
+import (
+	"context"
+	"log"
+	"log/slog"
+
+	"github.com/bookpanda/messenger-clone/internal/model"
+	"github.com/bookpanda/messenger-clone/pkg/logger"
+	pglib "github.com/bookpanda/messenger-clone/pkg/postgres"
+	rdlib "github.com/bookpanda/messenger-clone/pkg/redis"
+	"github.com/bookpanda/messenger-clone/pkg/storage"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type Store struct {
+	DB      *gorm.DB
+	Storage *storage.Client
+}
+
+func New(ctx context.Context, pgConfig pglib.Config, rdConfig rdlib.Config, storageConfig storage.Config) *Store {
+	db, err := gorm.Open(postgres.Open(pgConfig.String()), &gorm.Config{})
+	if err != nil {
+		logger.PanicContext(ctx, "failed to connect to database", slog.Any("error", err))
+	}
+
+	storage, err := storage.New(ctx, storageConfig)
+	if err != nil {
+		logger.PanicContext(ctx, "failed to connect to storage", slog.Any("error", err))
+	}
+
+	store := &Store{
+		DB:      db,
+		Storage: storage,
+	}
+	store.migrate()
+	return store
+}
+
+func (s *Store) migrate() {
+	log.Println("Running migrations...")
+
+	if err := s.DB.AutoMigrate(
+		&model.User{},
+		&model.Message{},
+	); err != nil {
+		logger.Panic("failed to migrate database", slog.Any("error", err))
+	}
+
+	log.Println("Migrations complete!")
+}
