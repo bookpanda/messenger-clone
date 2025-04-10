436bc60155cabd7a4f04680eca60820394386ee4
boomchanotai <boom9387@hotmail.com>
2025-04-10 09:18:40 +0700

    feat: chat info

--- client/src/components/chat-info.tsx
+++ client/src/components/chat-info.tsx
@@ -1,5 +1,14 @@
-import { Lock } from "lucide-react"
+import { Lock, Pin } from "lucide-react"
 import Image from "next/image"
+
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
+
+import { Button } from "./ui/button"
 
 export const ChatInfo = () => {
   return (
@@ -15,12 +24,58 @@ export const ChatInfo = () => {
         </div>
         <h2 className="font-medium">Chanotai Krajeam</h2>
       </div>
+
       <div className="flex justify-center">
         <div className="bg-chat-composer-input-background-color text-primary-foreground flex items-center gap-2 rounded-full px-3 py-1 text-xs font-medium">
           <Lock className="size-3" />
           <span>End-to-end encrypted</span>
         </div>
       </div>
+
+      <Accordion type="multiple" className="w-full">
+        <AccordionItem value="item-1" className="border-none">
+          <AccordionTrigger className="cursor-pointer rounded-md p-2.5 hover:bg-white/10 hover:no-underline">
+            Chat Info
+          </AccordionTrigger>
+          <AccordionContent className="p-0">
+            <Button
+              variant="ghost"
+              className="hover:text-primary-foreground h-auto w-full justify-start rounded-md p-2.5 hover:bg-white/10 hover:no-underline"
+            >
+              <Pin className="size-5" />
+              <span>View pinned messages</span>
+            </Button>
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-2" className="border-none">
+          <AccordionTrigger className="cursor-pointer rounded-md p-2.5 hover:bg-white/10 hover:no-underline">
+            Customize chat
+          </AccordionTrigger>
+          <AccordionContent className="p-0">
+            <Button
+              variant="ghost"
+              className="hover:text-primary-foreground h-auto w-full justify-start rounded-md p-2.5 hover:bg-white/10 hover:no-underline"
+            >
+              <Pin className="size-5" />
+              <span>Change themes</span>
+            </Button>
+            <Button
+              variant="ghost"
+              className="hover:text-primary-foreground h-auto w-full justify-start rounded-md p-2.5 hover:bg-white/10 hover:no-underline"
+            >
+              <Pin className="size-5" />
+              <span>Change emoji</span>
+            </Button>
+            <Button
+              variant="ghost"
+              className="hover:text-primary-foreground h-auto w-full justify-start rounded-md p-2.5 hover:bg-white/10 hover:no-underline"
+            >
+              <Pin className="size-5" />
+              <span>Edit nicknames</span>
+            </Button>
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
     </div>
   )
 }
