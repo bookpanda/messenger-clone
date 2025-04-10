f5533af9120c61e50f713948868cd8d3e5404359
Idhibhat Pankam <marginpankam@gmail.com>
2025-04-10 14:34:22 +0700

    server

---
+++ server/internal/server/register_routes.go
@@ -0,0 +1,17 @@
+package server
+
+import "github.com/bookpanda/messenger-clone/internal/services/auth"
+
+func (s *Server) RegisterRoutes(
+	authHandler *auth.Handler,
+
+) {
+	v1 := s.app.Group("/api/v1")
+
+	// auth
+	auth := v1.Group("/auth")
+	auth.Post("/login", authHandler.HandleLogin)
+	auth.Post("/register", authHandler.HandleRegister)
+	auth.Post("/refresh-token", authHandler.HandleRefreshToken)
+
+}
