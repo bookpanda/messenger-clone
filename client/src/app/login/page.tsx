96c77a6d6915aba8aebc411a08cdb381b1912135
Idhibhat Pankam <marginpankam@gmail.com>
2025-04-11 01:26:45 +0700

    ogin btn

--- client/src/app/login/page.tsx
+++ client/src/app/login/page.tsx
@@ -1,7 +1,11 @@
+import { Button } from "@/components/ui/button"
+
 export default function Login() {
   return (
     <div className="bg-secondary-background flex h-dvh flex-col overflow-hidden">
-      Login
+      <Button variant="outline" className="w-20">
+        Login
+      </Button>
     </div>
   )
 }
