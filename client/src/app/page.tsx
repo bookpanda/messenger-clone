97e2ffc625c90e3a5c625330ea58c2e8cd7bc5ea
boomchanotai <boom9387@hotmail.com>
2025-04-10 08:59:20 +0700

    feat: chat ui

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,18 +1,21 @@
-import { Header } from "@/components/header";
-import { Sidebar } from "@/components/sidebar";
+import { Chat } from "@/components/chat"
+import { Header } from "@/components/header"
+import { Sidebar } from "@/components/sidebar"
 
 export default function Home() {
   return (
     <div className="bg-secondary-background flex h-dvh flex-col overflow-hidden">
       <Header />
 
-      <div className="flex-1 flex">
+      <div className="flex flex-1">
         {/* Sidebar */}
         <Sidebar />
 
         {/* Body */}
-        <div className="flex-1"></div>
+        <div className="flex-1 p-4">
+          <Chat />
+        </div>
       </div>
     </div>
-  );
+  )
 }
---
+++ client/src/components/chat.tsx
@@ -0,0 +1,77 @@
+import { Images, Info, Phone, ThumbsUp, Video } from "lucide-react"
+import Image from "next/image"
+
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+
+export const Chat = () => {
+  return (
+    <div className="bg-primary-background text-primary-foreground flex h-full flex-col overflow-hidden rounded-md">
+      {/* Header */}
+      <div className="bg-primary-background flex flex-initial justify-between px-4 py-3 shadow">
+        <div className="flex items-center gap-2">
+          <div className="relative size-10">
+            <Image
+              src="/thumbnail.jpg"
+              alt=""
+              fill
+              className="rounded-full object-cover"
+            />
+          </div>
+          <div>
+            <h2>Chanotai Krajeam</h2>
+            <p className="text-secondary-text text-xs">Active 3h ago</p>
+          </div>
+        </div>
+        <div className="flex items-center gap-2">
+          <Button
+            size="icon"
+            className="text-primary-icon rounded-full"
+            variant="ghost"
+          >
+            <Phone className="size-5" />
+          </Button>
+          <Button
+            size="icon"
+            className="text-primary-icon rounded-full"
+            variant="ghost"
+          >
+            <Video className="size-5" />
+          </Button>
+          <Button
+            size="icon"
+            className="text-primary-icon rounded-full"
+            variant="ghost"
+          >
+            <Info className="size-5" />
+          </Button>
+        </div>
+      </div>
+      {/* Body */}
+      <div className="flex-1"></div>
+      {/* Message Input */}
+      <div className="flex flex-initial items-center gap-2 px-4 py-3">
+        <Button
+          size="icon"
+          className="text-chat-composer-button-color size-auto rounded-full p-1"
+          variant="ghost"
+        >
+          <Images className="size-5" />
+        </Button>
+        <div className="flex-1">
+          <Input
+            className="bg-chat-composer-input-background-color text-primary-text rounded-full border-none ring-0 outline-none"
+            placeholder="Aa"
+          />
+        </div>
+        <Button
+          size="icon"
+          className="text-chat-composer-button-color size-auto rounded-full p-1"
+          variant="ghost"
+        >
+          <ThumbsUp className="size-5" />
+        </Button>
+      </div>
+    </div>
+  )
+}
