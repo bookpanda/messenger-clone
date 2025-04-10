027626bb052a3cd9883534e35aeb057c29760c9b
boomchanotai <boom9387@hotmail.com>
2025-04-10 12:42:22 +0700

    feat: btn

--- client/src/components/chat/messages.tsx
+++ client/src/components/chat/messages.tsx
@@ -1,5 +1,8 @@
 import { cn } from "@/lib/utils"
+import { EllipsisVertical, Smile } from "lucide-react"
 import Image from "next/image"
+
+import { Button } from "@/components/ui/button"
 
 interface MessageProps {
   reaction?: string
@@ -10,7 +13,7 @@ export const IncomingMessage = (props: MessageProps) => {
 
   return (
     <div
-      className={cn("flex items-end gap-2", {
+      className={cn("group flex items-end gap-2", {
         "pb-2": reaction,
       })}
     >
@@ -22,13 +25,31 @@ export const IncomingMessage = (props: MessageProps) => {
           className="rounded-full object-cover"
         />
       </div>
-      <div className="bg-chat-incoming-message-bubble-background-color relative rounded-full px-3 py-2">
-        <p>แต่กุศรัทธาใน work life unbalance</p>
-        {reaction && (
-          <div className="bg-primary-background absolute right-3 -bottom-3 z-10 flex size-5 items-center justify-center rounded-full">
-            {reaction}
-          </div>
-        )}
+      <div className="flex items-center gap-2">
+        <div className="bg-chat-incoming-message-bubble-background-color relative rounded-full px-3 py-2">
+          <p>แต่กุศรัทธาใน work life unbalance</p>
+          {reaction && (
+            <div className="bg-primary-background absolute right-3 -bottom-3 z-10 flex size-5 items-center justify-center rounded-full">
+              {reaction}
+            </div>
+          )}
+        </div>
+        <div className="hidden items-center gap-2 group-hover:flex">
+          <Button
+            size="icon"
+            variant="secondary"
+            className="bg-secondary-background text-primary-foreground size-auto rounded-full p-1 hover:bg-white/10"
+          >
+            <Smile className="size-5" />
+          </Button>
+          <Button
+            size="icon"
+            variant="secondary"
+            className="bg-secondary-background text-primary-foreground size-auto rounded-full p-1 hover:bg-white/10"
+          >
+            <EllipsisVertical className="size-5" />
+          </Button>
+        </div>
       </div>
     </div>
   )
@@ -39,17 +60,35 @@ export const OutgoingMessage = (props: MessageProps) => {
 
   return (
     <div
-      className={cn("flex items-end justify-end gap-2", {
+      className={cn("group flex items-end justify-end gap-2", {
         "pb-2": reaction,
       })}
     >
-      <div className="bg-chat-outgoing-message-bubble-background-color relative rounded-full px-3 py-2">
-        <p>แต่กุศรัทธาใน work life unbalance</p>
-        {reaction && (
-          <div className="bg-primary-background absolute right-3 -bottom-3 z-10 flex size-5 items-center justify-center rounded-full">
-            {reaction}
-          </div>
-        )}
+      <div className="flex items-center gap-2">
+        <div className="hidden items-center gap-2 group-hover:flex">
+          <Button
+            size="icon"
+            variant="secondary"
+            className="bg-secondary-background text-primary-foreground size-auto rounded-full p-1 hover:bg-white/10"
+          >
+            <EllipsisVertical className="size-5" />
+          </Button>
+          <Button
+            size="icon"
+            variant="secondary"
+            className="bg-secondary-background text-primary-foreground size-auto rounded-full p-1 hover:bg-white/10"
+          >
+            <Smile className="size-5" />
+          </Button>
+        </div>
+        <div className="bg-chat-outgoing-message-bubble-background-color relative rounded-full px-3 py-2">
+          <p>แต่กุศรัทธาใน work life unbalance</p>
+          {reaction && (
+            <div className="bg-primary-background absolute right-3 -bottom-3 z-10 flex size-5 items-center justify-center rounded-full">
+              {reaction}
+            </div>
+          )}
+        </div>
       </div>
     </div>
   )
