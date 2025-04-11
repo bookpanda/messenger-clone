b8fb1ed92e97e4c1cb71782730b96bd61db5e518
Idhibhat Pankam <marginpankam@gmail.com>
2025-04-11 21:54:46 +0700

    fix click

--- client/src/components/header/index.tsx
+++ client/src/components/header/index.tsx
@@ -7,19 +7,20 @@ import { ProfilePicture } from "./profile-picture"
 
 export const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false)
-  const menuRef = useRef<HTMLDivElement>(null)
+  const wrapperRef = useRef<HTMLDivElement>(null)
 
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
-      if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+      if (
+        wrapperRef.current &&
+        !wrapperRef.current.contains(event.target as Node)
+      ) {
         setIsMenuOpen(false)
       }
     }
 
     if (isMenuOpen) {
       document.addEventListener("mousedown", handleClickOutside)
-    } else {
-      document.removeEventListener("mousedown", handleClickOutside)
     }
 
     return () => {
@@ -29,13 +30,15 @@ export const Header = () => {
 
   return (
     <div className="bg-primary-background text-primary-foreground flex-initial border-b border-white/20 px-4">
-      <div className="flex items-center justify-between py-2" ref={menuRef}>
-        <div className="size-10">
-          <Icon icon="logos:facebook" className="size-full" />
+      <div ref={wrapperRef}>
+        <div className="flex items-center justify-between py-2">
+          <div className="size-10">
+            <Icon icon="logos:facebook" className="size-full" />
+          </div>
+          <ProfilePicture onClick={() => setIsMenuOpen(!isMenuOpen)} />
         </div>
-        <ProfilePicture onClick={() => setIsMenuOpen(!isMenuOpen)} />
+        {isMenuOpen && <ProfileMenu />}
       </div>
-      {isMenuOpen && <ProfileMenu />}
     </div>
   )
 }
