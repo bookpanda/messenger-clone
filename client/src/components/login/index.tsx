2c79bf17403bacf227a87db586e6da0da8356814
Idhibhat Pankam <marginpankam@gmail.com>
2025-04-11 16:18:59 +0700

    rediect

--- client/src/components/login/index.tsx
+++ client/src/components/login/index.tsx
@@ -9,7 +9,8 @@ import { Button } from "../ui/button"
 export default function LoginPageComponent() {
   const handleLogin = async () => {
     await signIn("google", {
-      redirectTo: "/",
+      redirect: true,
+      redirectTo: "/api/auth/callback/google",
     })
   }
 
