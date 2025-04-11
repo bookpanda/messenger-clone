93712d413c83e26688e21aa2b9bc2b25e802f7e1
Idhibhat Pankam <marginpankam@gmail.com>
2025-04-11 21:14:26 +0700

    profile chevron

--- client/src/components/header/profile-picture.tsx
+++ client/src/components/header/profile-picture.tsx
@@ -1,11 +1,11 @@
 "use client"
 
-import { signOut, useSession } from "next-auth/react"
+import { Icon } from "@iconify/react"
+import { useSession } from "next-auth/react"
 import Image from "next/image"
 import { redirect } from "next/navigation"
 
 export const ProfilePicture = () => {
-  // getSessio
   const { data: session, status } = useSession()
 
   if (status === "loading") {
@@ -16,16 +16,21 @@ export const ProfilePicture = () => {
     redirect("/login")
   }
 
-  console.log(session)
   return (
-    <div>
+    <div className="relative cursor-pointer">
       <Image
         alt="profile-pic"
         src={session?.user?.image}
-        height={50}
-        width={50}
-        className="cursor-pointer rounded-full"
+        height={40}
+        width={40}
+        className="rounded-full"
       />
+      <div className="border-primary-background absolute right-0 bottom-0 h-4 w-4 rounded-full border-2 bg-gray-600">
+        <Icon
+          icon="fluent:chevron-down-12-filled"
+          className="size-full text-gray-200"
+        />
+      </div>
     </div>
   )
 }
