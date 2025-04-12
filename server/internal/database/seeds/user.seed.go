5703ff98b20f1d53c2c2dda88fea659ff97fd891
Idhibhat Pankam <marginpankam@gmail.com>
2025-04-13 02:30:02 +0700

    seed

---
+++ server/internal/database/seeds/user.seed.go
@@ -0,0 +1,38 @@
+package seeds
+
+import (
+	"github.com/bookpanda/messenger-clone/internal/model"
+)
+
+var Users = []model.User{
+	{
+		Name:              "John Doe",
+		Email:             "johndoe@gmail.com",
+		ProfilePictureURL: "https://avatar.iran.liara.run/username?username=John+Doe",
+	},
+	{
+		Name:              "Bob Builder",
+		Email:             "bobdabuilder@gmail.com",
+		ProfilePictureURL: "https://avatar.iran.liara.run/username?username=Bob+Builder",
+	},
+	{
+		Name:              "Tralalero Tralala",
+		Email:             "tralalerotralala@gmail.com",
+		ProfilePictureURL: "https://avatar.iran.liara.run/username?username=Tralalero+Tralala",
+	},
+	{
+		Name:              "Bombardiro Crocodillo",
+		Email:             "bombardirocrocodillo@gmail.com",
+		ProfilePictureURL: "https://avatar.iran.liara.run/username?username=Bombardiro+Crocodillo",
+	},
+	{
+		Name:              "Tungguk",
+		Email:             "tungtungtungtungtungtungtung@gmail.com",
+		ProfilePictureURL: "https://avatar.iran.liara.run/username?username=Tungguk+Sahur",
+	},
+	{
+		Name:              "Capuccino Asashino",
+		Email:             "capuccinoasashino@gmail.com",
+		ProfilePictureURL: "https://avatar.iran.liara.run/username?username=Capuccino+Asashino",
+	},
+}
