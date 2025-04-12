6ad9b3f2b0f6ad6b8f5c2139aa6fe886afaa6bef
Idhibhat Pankam <marginpankam@gmail.com>
2025-04-13 01:59:48 +0700

    chat prtiunique key

--- server/internal/model/chat_participant.go
+++ server/internal/model/chat_participant.go
@@ -4,9 +4,9 @@ import "gorm.io/gorm"
 
 type ChatParticipant struct {
 	gorm.Model
-	ChatID uint `gorm:"not null"`
+	ChatID uint `gorm:"not null;index:idx_chat_user,unique"`
 	Chat   Chat `gorm:"foreignKey:ChatID"`
 
-	UserID uint `gorm:"not null"`
+	UserID uint `gorm:"not null;index:idx_chat_user,unique"`
 	User   User `gorm:"foreignKey:UserID"`
 }
