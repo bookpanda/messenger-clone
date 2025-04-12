2ef7d1821de5b8907061cefc9c9efddcf33d58c0
Idhibhat Pankam <marginpankam@gmail.com>
2025-04-13 02:38:56 +0700

    basic seeder

--- server/internal/database/seeds/seeder.go
+++ server/internal/database/seeds/seeder.go
@@ -10,19 +10,41 @@ import (
 func Execute(db *gorm.DB) error {
 	log.Println("Running seeder...")
 
-	userCount := 0
-	for _, user := range Users {
-		result := db.Model(&model.User{}).FirstOrCreate(&user)
-		if result.Error != nil {
-			log.Printf("Failed to seed user: %v\n", result.Error)
-			return result.Error
-		}
-		if result.RowsAffected > 0 {
-			userCount++
-		}
+	if err := seedModel(db, &model.User{}, toInterfaceSlice(Users)); err != nil {
+		return err
 	}
-	log.Printf("Seeded %d users\n", userCount)
+	if err := seedModel(db, &model.Chat{}, toInterfaceSlice(Chats)); err != nil {
+		return err
+	}
+	if err := seedModel(db, &model.ChatParticipant{}, toInterfaceSlice(Participants)); err != nil {
+		return err
+	}
 
 	log.Println("All seeder done")
 	return nil
+}
+
+func seedModel(db *gorm.DB, model interface{}, data []interface{}) error {
+	chatCount := 0
+	for _, item := range data {
+		result := db.Model(model).Create(item)
+		if result.Error != nil {
+			log.Printf("Failed to seed %T: %v\n", model, result.Error)
+			return result.Error
+		}
+		if result.RowsAffected > 0 {
+			chatCount++
+		}
+	}
+
+	log.Printf("Seeded %d chats\n", chatCount)
+	return nil
+}
+
+func toInterfaceSlice[T any](items []T) []interface{} {
+	result := make([]interface{}, len(items))
+	for i, v := range items {
+		result[i] = &v
+	}
+	return result
 }
