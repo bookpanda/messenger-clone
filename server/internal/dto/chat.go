3284273e1b1a9c0894bfe37d99a7c2f3d9cdf208
Idhibhat Pankam <marginpankam@gmail.com>
2025-04-13 01:20:14 +0700

    mod part dto

--- server/internal/dto/chat.go
+++ server/internal/dto/chat.go
@@ -6,6 +6,37 @@ type CreateChatRequest struct {
 }
 
 type CreateChatResponse struct {
-	ID   uint   `json:"id"`
-	Name string `json:"name"`
+	ID           uint           `json:"id"`
+	Name         string         `json:"name"`
+	Participants []UserResponse `json:"participants"`
+}
+
+type ParticipantAction string
+
+const (
+	AddParticipant    ParticipantAction = "ADD"
+	RemoveParticipant ParticipantAction = "REMOVE"
+)
+
+func (p ParticipantAction) String() string {
+	return string(p)
+}
+
+func ValidateParticipantAction(participantAction string) bool {
+	switch ParticipantAction(participantAction) {
+	case AddParticipant, RemoveParticipant:
+		return true
+	}
+	return false
+}
+
+type ModifyParticipantRequest struct {
+	Action       string   `json:"action" validate:"required"`
+	Participants []string `json:"participants" validate:"required"`
+}
+
+type ModifyParticipantResponse struct {
+	ID           uint           `json:"id"`
+	Name         string         `json:"name"`
+	Participants []UserResponse `json:"participants"`
 }
