bb64b598a15fe78ceafcc93e3e4c8db6f69e75a5
Idhibhat Pankam <marginpankam@gmail.com>
2025-04-13 22:31:20 +0700

    skeleton

--- client/src/components/messenger/sidebar/people-tab.tsx
+++ client/src/components/messenger/sidebar/people-tab.tsx
@@ -1,19 +1,33 @@
 import { useGetAllUsers } from "@/hooks/use-get-all-users"
+import { useSession } from "next-auth/react"
 
-import { ChatCard } from "./card/chat-card"
+import { Skeleton } from "@/components/ui/skeleton"
+
 import { PeopleCard } from "./card/people-card"
 
 export const PeopleTab = () => {
   const { users, loading } = useGetAllUsers()
+  const { data: session } = useSession()
 
   if (loading) {
-    return <p>loading</p>
+    return (
+      <div className="flex flex-col gap-1">
+        {Array.from({ length: 10 }).map((_, idx) => (
+          <Skeleton key={idx} className="bg-muted-foreground/30 h-18 w-full" />
+        ))}
+      </div>
+    )
   }
 
   return (
     <div>
       {users.map((user) => {
-        if (!user.id || !user.name || !user.profilePictureUrl) {
+        if (
+          !user.id ||
+          !user.name ||
+          !user.profilePictureUrl ||
+          user.email === session?.user?.email
+        ) {
           return null
         }
         return (
@@ -25,14 +39,6 @@ export const PeopleTab = () => {
           />
         )
       })}
-      <ChatCard
-        name="Chanotai Krajeam"
-        image="/thumbnail.jpg"
-        lastMessage="hi"
-        lastMessageFromMe={true}
-        lastMessageDate={new Date()}
-        isActive={true}
-      />
     </div>
   )
 }
