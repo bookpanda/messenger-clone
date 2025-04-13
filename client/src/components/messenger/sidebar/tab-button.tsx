a620e7ae46c1413abdd3cf399a2f37cb44796b2f
Idhibhat Pankam <marginpankam@gmail.com>
2025-04-13 16:47:09 +0700

    sodn

--- client/src/components/messenger/sidebar/tab-button.tsx
+++ client/src/components/messenger/sidebar/tab-button.tsx
@@ -9,8 +9,10 @@ export const TabButton = ({ isActive, onClick, children }: TabButtonProps) => {
   return (
     <button
       onClick={onClick}
-      className={`text-md cursor-pointer rounded-full px-3 py-1.5 font-bold hover:bg-white/10 ${
-        isActive && "bg-blue-500/20 text-blue-500 hover:bg-blue-500/20"
+      className={`text-md cursor-pointer rounded-full px-3 py-1.5 font-bold transition-colors ${
+        isActive
+          ? "bg-blue-500/20 text-blue-300 hover:bg-blue-500/30"
+          : "text-white hover:bg-white/10"
       }`}
     >
       {children}
