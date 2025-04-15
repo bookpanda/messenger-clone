2f94f1c8795514f58611336bd2ae986e14bd143b
Idhibhat Pankam <marginpankam@gmail.com>
2025-04-15 15:17:51 +0700

    fix hydration

--- client/src/app/providers.tsx
+++ client/src/app/providers.tsx
@@ -1,11 +1,19 @@
 "use client"
 
-import { ReactNode } from "react"
+import { ReactNode, useEffect, useState } from "react"
 
 import { ChatProvider } from "@/contexts/chat-provider"
 import { SessionProvider } from "next-auth/react"
 
 export default function Providers({ children }: { children: ReactNode }) {
+  const [mounted, setMounted] = useState(false)
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) return null
+
   return (
     <SessionProvider>
       <ChatProvider>{children}</ChatProvider>
