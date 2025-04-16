643b84173a07216fe5a41185526400703ddf35ef
boomchanotai <boom9387@hotmail.com>
2025-04-16 21:41:15 +0700

    fix: styling

--- client/src/components/login/index.tsx
+++ client/src/components/login/index.tsx
@@ -17,9 +17,11 @@ export default function LoginPageComponent() {
     <div className="bg-primary-background flex min-h-screen flex-1 flex-col items-center justify-between py-32">
       <div className="flex flex-col items-center gap-6">
         <Icon icon="logos:messenger" className="size-16" />
-        <div className="text-center">
-          <h1 className="text-xl font-bold text-white">Welcome to Messenger</h1>
-          <p className="text-sm font-medium text-gray-600">
+        <div className="space-y-1 text-center">
+          <h1 className="text-2xl font-bold text-white">
+            Welcome to Messenger
+          </h1>
+          <p className="font-medium text-gray-600">
             The simple way to text, call and video chat right from your desktop
           </p>
         </div>
